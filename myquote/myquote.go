ee4a3c1a21024a633586186fb8b2255912eacba3
unknown <Markus>
2022-01-28 12:42:04 +0100

    Fjernet ting

--- myquote/myquote.go
+++ myquote/myquote.go
@@ -9,3 +9,7 @@ func PrintQuotes() {
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
 }
+
+// Her er koden du ville kjørt for å lage en go.mod fil som andre //
+//kan hente ting fra
+//go mod init github.com/MarkusYohandsome/IS-105-Module1
